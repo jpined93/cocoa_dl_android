d9a335c10659820a03379a8e21a5c7c9c43bfb44
jpined93 <101982334+jpined93@users.noreply.github.com>
2022-12-04 19:50:29 -0500

    Add files via upload

--- FlaskWebService/src/app.py
+++ FlaskWebService/src/app.py
@@ -3,15 +3,21 @@ from pathlib import Path
 import flask
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from flask_restful import Api
-from dl_model.image_service import Image,Health
+from flask_restful import Api,reqparse
+from dl_model.image_service import Image,Health,PostImage
 
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Health, '/h')
-api.add_resource(Image, '/image')
 
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+
+api.add_resource(Health, '/')
+api.add_resource(Image, '/image/<json_image>')
+api.add_resource(PostImage, '/l_img')
 
 jwt = JWTManager(app)
