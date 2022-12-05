29f093db28428d809dc7e170296bf598ef5562df
jpined93 <101982334+jpined93@users.noreply.github.com>
2022-12-04 19:48:57 -0500

    Add files via upload

--- FlaskWebService/src/dl_model/image_service.py
+++ FlaskWebService/src/dl_model/image_service.py
@@ -5,10 +5,12 @@ import traceback
 import json
 from flask import request, jsonify, send_file
 from datetime import datetime
-from flask_restful import Resource
+from flask_restful import Resource,reqparse
 from flask import current_app as app
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-
+from dl_model.load_model import load,model_predict
+import tensorflow as tf
+import keras 
 
 
 class Health(Resource):    
@@ -16,5 +18,33 @@ class Health(Resource):
         return {"resultado": "OK", "mensaje": "service is alive"}, 200
 
 class Image(Resource):
-    def get(self):
-        return {"resultado": "OK", "mensaje": "service is alive"}, 200
+
+    def get(self,json_image):
+        from PIL import Image
+        from io import BytesIO
+        import base64
+        import json
+        
+        MODEL_PATH="C:/Users/Lobo_/Desktop/Cocoa_DL/cocoa_dl_android/FlaskWebService/src/dl_model/"
+        model=load(MODEL_PATH)
+
+
+        y = json.loads(json_image)
+        img = Image.open(BytesIO(base64.b64decode(y["img"])))
+        
+
+        #preds=model_predict(img,model)
+        #preds=preds.tolist()[0]
+        return json_image #{"resultado":preds}, 200
+
+class PostImage(Resource):
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+
+
+        return json_image
--- FlaskWebService/src/dl_model/load_model.py
+++ FlaskWebService/src/dl_model/load_model.py
@@ -1,7 +1,33 @@
 import pickle
+import tensorflow as tf
+import keras
+from PIL import Image
+import numpy as np
 
 def load(model_path):
-    model = pickle.load(open('model.pkl','rb'))
+    # Loading the pretrained model
+    
+    Model_json = model_path+"model.json"
+    Model_weights = model_path+"model.h5"
+
+    model_json = open(Model_json, 'r')
+    loaded_model_json = model_json.read()
+    model_json.close()
+    model = tf.keras.models.model_from_json(loaded_model_json)
+    model.load_weights(Model_weights)
+
     return model
 
+
+def model_predict(img,model):
+    img = img.resize((300, 300))
+    # Preprocessing the image
+    x = tf.keras.utils.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+    # Be careful how your trained model deals with the input
+    # otherwise, it won’t make correct prediction!
+    #x = preprocess_input(x)
+    preds = model.predict(x)
+    return preds
 
