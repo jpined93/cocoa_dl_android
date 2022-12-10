240ede555d62d3fe8bc67115b2e9157c912f3e58
jpined93 <jpined93@gmail.com>
2022-12-09 21:39:21 -0500

    Functional Flask service

--- FlaskWebService/src/app.py
+++ FlaskWebService/src/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from pathlib import Path
 import flask
@@ -21,28 +20,4 @@ api.add_resource(Health, '/')
 api.add_resource(Image, '/image')
 api.add_resource(PostImage, '/l_img')
 
-=======
-import os
-from pathlib import Path
-import flask
-from flask import Flask
-from flask_jwt_extended import JWTManager
-from flask_restful import Api,reqparse
-from dl_model.image_service import Image,Health,PostImage
-
-
-app = Flask(__name__)
-api = Api(app)
-
-
-parser = reqparse.RequestParser()
-parser.add_argument('task')
-
-
-
-api.add_resource(Health, '/')
-api.add_resource(Image, '/image/<json_image>')
-api.add_resource(PostImage, '/l_img')
-
->>>>>>> d9a335c10659820a03379a8e21a5c7c9c43bfb44
 jwt = JWTManager(app)
--- README.md
+++ README.md
@@ -15,5 +15,10 @@ if not activate, allow scripts run with following command
 Set-ExecutionPolicy Unrestricted -Scope Process
 Activate virtual enviroment from FlaskWebService folder venv/Scripts/activate
 Activate Flask app enviroment $env:FLASK_APP ="C:\Users\Lobo_\Desktop\Cocoa_DL\cocoa_dl_android\FlaskWebService\src\app.py"
+
+or
+
+$env:FLASK_APP ="C:/Users/Lobo_/Desktop/Cocoa_DL/cocoa_dl_android/FlaskWebService/src/app_simple.py"
+
 on Virtual enviroment run py -m pip install -r requirements.txt if not already installed
 
