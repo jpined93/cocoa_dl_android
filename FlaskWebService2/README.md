9c860ed9362ceee31969476e630d6a930b2028e3
jpined93 <jpined93@gmail.com>
2023-05-13 17:28:08 -0500

    re design of app exception sistem

---
+++ FlaskWebService2/README.md
@@ -0,0 +1,7 @@
+Creación de imagen docker y levantamiento del servicio
+
+
+crear imagen-->docker build -t tesis_flask_service:0.0.1
+
+
+correr la imagen --> docker container run -d -p 5000:5000 tesis_flask_service:0.0.1
--- FlaskWebService2/src/app.py
+++ FlaskWebService2/src/app.py
@@ -30,35 +30,121 @@ model = tf.keras.models.model_from_json(loaded_model_json)
 model.load_weights(Model_weights)
 
 
+def decode_img(img):
+    try:
+        im_bytes = base64.b64decode(img)   # im_bytes is a binary image
+        im_file = BytesIO(im_bytes)  # convert image to file-like object
+        img = Image.open(im_file)   # img is now PIL Image object
+        img = img.resize((300, 300))
+        print ("image decoded")
+        return img 
+    except Exception as e:
+        print(f"Exception decoding img: {e}" )
+        return None
+    
+
+def preprocessing_img(img):
+    try:
+        x = tf.keras.utils.img_to_array(img)
+        # x = np.true_divide(x, 255)
+        x = np.expand_dims(x, axis=0)
+        print ("preprocess compleated")
+        return x
+    except Exception as e:
+        print(f"Exception preprocessing img: {e}" )
+        return None
+    
+def predict_img(x):
+    try:
+        preds = model.predict(x)
+        preds=preds.tolist()[0]
+        if np.argmax(np.array(preds))==0:
+            class_pred="Cat"
+        elif np.argmax(np.array(preds))==1:
+            class_pred="Dog"
+        
+        print ("image predicted")
+        return jsonify({"img":str(class_pred)})
+    except Exception as e:
+        print(f"Exception making predictions img: {e}" )
+        return None
+
 
 app=Flask(__name__)
 
 @app.route("/uImg", methods=['GET','POST'])
 def val_img():
-    if request.method=="POST":
-        d=request.get_data()
-        # Transfor image to PIL
-        #==============================================================================
+    try:
+        if request.method=="POST":
+            
+            
+            d=request.get_data()
+            try:
+                im_bytes = base64.b64decode(d)   # im_bytes is a binary image
+                im_file = BytesIO(im_bytes)  # convert image to file-like object
+                img = Image.open(im_file)   # img is now PIL Image object
+                img = img.resize((300, 300))
+                print ("image decoded")
+            except Exception as e:
+                print(f"Exception decoding img: {e}" )
+                return jsonify({f"error":f"Exception decoding img: {e}"})
+            
+            try:
+                x = tf.keras.utils.img_to_array(img)
+                # x = np.true_divide(x, 255)
+                x = np.expand_dims(x, axis=0)
+                print ("preprocess compleated")
+            except Exception as e:
+                print(f"Exception preprocessing img: {e}" )
+                return jsonify({f"error":f"Exception preprocessing img: {e}"})
 
-        im_bytes = base64.b64decode(d)   # im_bytes is a binary image
-        im_file = BytesIO(im_bytes)  # convert image to file-like object
-        img = Image.open(im_file)   # img is now PIL Image object
-        img = img.resize((300, 300))
-        # Preprocessing the image
-        x = tf.keras.utils.img_to_array(img)
-        # x = np.true_divide(x, 255)
-        x = np.expand_dims(x, axis=0)
-        # Be careful how your trained model deals with the input
-        # otherwise, it won’t make correct prediction!
-        preds = model.predict(x)
-        preds=preds.tolist()[0]
-        if preds[0]==1:
-            class_pred="Cat"
-        elif preds[1]==1:
-            class_pred="Dog"
-        return jsonify({"img":str(class_pred)})
-    elif request.method=="GET":
-        #data=request.args.get("img")
-        d=request.args.get("img")
-        return jsonify({"img":"get is working"})
+            try:
+                preds = model.predict(x)
+                preds=preds.tolist()[0]
+                if np.argmax(np.array(preds))==0:
+                    class_pred="Cat"
+                elif np.argmax(np.array(preds))==1:
+                    class_pred="Dog"
+                
+                print ("image predicted")
+                return jsonify({"img":str(class_pred)})
+            except Exception as e:
+                print(f"Exception making predictions img: {e}" )
+                return jsonify({f"error":f"Exception making predictions img: {e}"})
+
+
+
+            # Transfor image to PIL
+            #==============================================================================
+
+            # im_bytes = base64.b64decode(d)   # im_bytes is a binary image
+            # im_file = BytesIO(im_bytes)  # convert image to file-like object
+            # img = Image.open(im_file)   # img is now PIL Image object
+            # img = img.resize((300, 300))
+            
+
+            # # Preprocessing the image
+            # x = tf.keras.utils.img_to_array(img)
+            # # x = np.true_divide(x, 255)
+            # x = np.expand_dims(x, axis=0)
+            # # Be careful how your trained model deals with the input
+            # # otherwise, it won’t make correct prediction!
+
+            
+
+            # preds = model.predict(x)
+            # preds=preds.tolist()[0]
+            # if preds[0]==1:
+            #     class_pred="Cat"
+            # elif preds[1]==1:
+            #     class_pred="Dog"
+            # return jsonify({"img":str(class_pred)})
+            
+        elif request.method=="GET":
+            #data=request.args.get("img")
+            d=request.args.get("img")
+            return jsonify({"img":"get is working"})
+    except Exception as e:
+        print(e)
+
 app.run(debug=True)
